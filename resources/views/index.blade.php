a581630e6dc27d26780a9fc1b5af9e5f852b8290
duynamdeveloper <duynam.dev223@gmail.com>
2017-09-19 17:52:08 +0700

    new

---
+++ resources/views/index.blade.php
@@ -0,0 +1,442 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <!-- Theme Made By www.w3schools.com - No Copyright -->
+  <title>IMAS 2017</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet" type="text/css">
+  <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet" type="text/css">
+   <link href="https://fonts.googleapis.com/css?family=Comfortaa:300,400,700&amp;subset=vietnamese" rel="stylesheet"> 
+  <link href="{{asset('/css/styles.css')}}" rel="stylesheet" type="text/css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+ </head> 
+<body id="homepage" data-spy="scroll" data-target=".navbar" data-offset="100">
+
+<nav class="navbar navbar-default" id="masthead">
+  <div class="container">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>                        
+      </button>
+      <a class="navbar-brand" href="#homepage"></a>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#about">Giới thiệu</a></li>
+        <li><a href="#purposes">Mục đích</a></li>
+        <li><a href="#location">Thời gian</a></li>
+        <li><a href="#exam">Đề thi</a></li>
+        <li><a href="#prize">Giải thưởng</a></a></li>
+        <li><a href="#contact">Liên hệ</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
+<div id="particles-js">
+<div class="text-center page-header-title">
+  <h1>IMAS</h1> 
+  <p>International Mathematics Assesments for Schools</p> 
+ 
+</div>
+ </div>
+
+
+<!-- Container (About Section) -->
+<div id="about" class="container-fluid">
+  <div class="row slideanim">
+    <div class="col-sm-6">
+      <h2>Giới thiệu</h2><br>
+     
+      <p><strong>Kỳ thi đánh giá năng lực tư duy Toán học quốc tế - IMAS (International mathematics
+assessments for school)</strong> là kỳ thi đánh giá năng lực tư duy Toán học bằng tiếng Anh dành
+riêng cho học sinh Tiểu học và Trung học cơ sở. Bài thi đánh giá tư duy Toán toàn diện trên
+3 góc độ: Hiểu kiến thức, Áp dụng thực tế và Năng lực lập luận trên nền tảng tiếng Anh.<br>
+Kỳ thi sử dụng bài thi theo chuẩn quốc tế do Ủy ban điều hành IMAS quốc tế biên soạn.
+Ban tổ chức và điều hành IMAS bao gồm đại diện các hiệp hội Toán học, các trường Đại
+học, Viện nghiên cứu và các Tổ chức giáo dục uy tín trên thế giới. Đây cũng là ban tổ chức
+đã thực hiện nhiều cuộc thi trong khu vực và trên thế giới như kỳ thi “Olympic Toán và Khoa
+học quốc tế – (International Mathematics and Science Olympiad – IMSO)”; kỳ thi “Toán học
+trẻ quốc tế (International Mathematics Competition – IMC)”.<br>
+Kỳ thi được tổ chức thường niên tại nhiều quốc gia trên thế giới như Bun-ga-ri, Đài Loan,
+Indonesia, Malaysia, Nam Phi, Phi-lip-pin, Ru-ma-ni, Thái Lan, Úc, Trung Quốc, và Zim-ba-
+buê.</p>
+     
+    </div>
+    <div class="col-sm-6">
+      <img src="{{asset('imgs/IMG_6481.png')}}" width="100%" height="400px">
+    </div>
+  </div>
+</div>
+
+<div class="container-fluid bg-grey" id="purposes">
+  <div class="row slideanim">
+    <div class="col-sm-6">
+      <img src="{{asset('imgs/4.jpg')}}" class="img img-responsive" width="100%" height="400px">
+    </div>
+    <div class="col-sm-6">
+      <h2>Mục đích - Ý nghĩa</h2><br>
+      <p>
+     <span class="glyphicon glyphicon-ok text-success"></span> Cung cấp một hệ thống đánh giá năng lực Toán học và một cuộc thi thử thách trí tuệ
+thông qua Toán học cho các học sinh. <br>
+<span class="glyphicon glyphicon-ok text-success"></span> Phát triển hệ thống đánh giá kiểm tra năng lực Toán học tầm cỡ quốc tế để đo
+lường năng lực của học sinh trên cả 3 góc độ: Kiến thức (Knowing); Khả năng áp
+dụng (Applying); Khả năng lý luận (Reasoning).<br>
+<span class="glyphicon glyphicon-ok text-success"></span> Thúc đẩy việc học tập Toán học và phát triển Tư duy một cách hiệu quả tại cả 2
+cấp Tiểu học và Trung học cơ sở thông qua một nền tảng đo lường và đánh giá
+kết quả được công nhận rộng rãi ở cấp độ quốc tế.<br>
+<span class="glyphicon glyphicon-ok text-success"></span> Thông tin cho học sinh, giáo viên, nhà trường và gia đình về năng lực Toán học
+đạt được của học sinh ở các cấp độ khác nhau.<br>
+<span class="glyphicon glyphicon-ok text-success"></span> Cung cấp cho học sinh một hệ thống đánh giá thân thiện với nhiều chủ đề và các
+cấp độ nâng cao khác nhau.<br>
+<span class="glyphicon glyphicon-ok text-success"></span> Tạo lập một phương thức đánh giá sáng tạo với việc thí sinh được lựa chọn cấp độ khó
+của bài thi. Thí sinh được chủ động trong việc đánh giá khả năng và năng lực của mình
+      </p>
+    </div>
+  </div>
+</div>
+
+<!-- Container (Services Section) -->
+<div id="location" class="container-fluid text-center">
+  <h2>Thời gian - Địa điểm</h2>
+  <h4>Thời gian - địa điểm tổ chức kỳ thi IMAS 2017 được dự kiến như sau:</h4>
+  <br>
+  <div class="row slideanim">
+    <div class="col-sm-8 col-sm-offset-2">
+      <table class="table table-bordered yellow-heading" id="time-location-table">
+        <thead>
+        <th>Nội dung</th>
+        <th text-align="center">Thời gian</th>
+        <th>Địa điểm</th>
+        </thead>
+        <tbody>
+          <tr>
+            <td align="left"> <strong>Phát động kỳ thi </strong></td>
+            <td align="center">28/08/2017</td>
+            <td align="left">Hà Nội</td>
+          </tr>
+           <tr>
+            <td align="left"><strong>Hạn nhận đăng ký</strong></td>
+            <td>30/09/2017</td>
+            <td align="left">Hà Nội</td>
+          </tr>
+           <tr>
+            <td align="left"><strong>Ngày thi vòng 1 (dự kiến)</strong></td>
+            <td>18/01/2018</td>
+            <td align="left"><strong>Các điểm thi theo thông báo</strong></td>
+          </tr>
+           <tr>
+            <td align="left"><strong>Thông báo kết quả thi vòng 1</strong></td>
+            <td>05/02/2018</td>
+            <td align="left">Website kỳ thi</td>
+          </tr>
+           <tr>
+            <td align="left"><strong>Ngày thi vòng 2</strong></td>
+            <td>01/04/2018</td>
+            <td align="left">Theo thông báo</td>
+          </tr>
+           <tr>
+            <td align="left"><strong>Lễ trao giải (dự kiến)</strong></td>
+            <td>13/05/2018</td>
+            <td align="left">Hà Nội</td>
+          </tr>
+          
+        </tbody>
+      </table>
+    </div>
+  </div>
+</div>
+
+<!-- Container (Portfolio Section) -->
+<div id="exam" class="container-fluid text-center bg-grey">
+  <h2>Cấu trúc đề thi</h2><br>
+  <h4>Kỳ thi sử dụng <strong>Đề thi bằng tiếng Anh</strong> do Ủy ban điều hành IMAS quốc tế cung cấp.</h4>
+  <div class="row text-center slideanim">
+        <div class="col-sm-6 col-xs-12">
+      <div class="panel panel-default text-center">
+        <div class="panel-heading">
+          <h2 class="text-white">Vòng 1</h2>
+        </div>
+        <div class="panel-body">
+          <p>Có tất cả 25 câu hỏi với thời gian làm bài là 75 phút. 20 câu hỏi đầu tiên thuộc
+dạng trắc nghiệm, 5 câu hỏi còn lại yêu cầu các câu trả lời là số nguyên từ 0 đến 999. Độ
+khó của các câu hỏi được phân bổ như sau:</p>
+        </div>
+        <div class="panel-footer">
+           <table class="table table-bordered yellow-heading">
+        <thead>
+        <th>Câu hỏi</th>
+        <th text-align="center">Hình thức đề bài</th>
+        <th>Điểm số</th>
+        </thead>
+        <tbody>
+          <tr>
+            <td align="left"> <strong>Câu 1 - 10 </strong></td>
+            <td align="left">Câu hỏi dễ</td>
+            <td align="left">Mỗi câu trả lời đúng thí sinh được 3 điểm</td>
+          </tr>
+           <tr>
+            <td align="left"><strong>Câu 11 - 20</strong></td>
+            <td align="left">Câu hỏi trung bình</td>
+            <td align="left">Mỗi câu trả lời đúng thí sinh được 4 điểm</td>
+          </tr>
+           <tr>
+            <td align="left"><strong>Câu 21 – 25</strong></td>
+            <td align="left">Câu hỏi khó</td>
+            <td align="left">Mỗi câu trả lời đúng thí sinh được 6 điểm</td>
+          </tr>
+          
+          
+        </tbody>
+      </table>
+      <br>
+      <br>
+        </div>
+      </div>      
+    </div> 
+      <div class="col-sm-6 col-xs-12">
+      <div class="panel panel-default text-center">
+        <div class="panel-heading">
+          <h2 class="text-white">Vòng 2</h2>
+        </div>
+        <div class="panel-body">
+          <p>Dành cho <strong>top 5% đến 10% </strong>các thí sinh đạt kết quả cao nhất ở Vòng 1.<br>
+Có tất cả 15 câu hỏi:</p>
+<br>
+        </div>
+        <div class="panel-footer">   <table class="table table-bordered yellow-heading">
+        <thead>
+        <th>Câu hỏi</th>
+        <th text-align="center">Hình thức đề bài</th>
+        <th>Điểm số</th>
+        </thead>
+        <tbody>
+          <tr>
+            <td align="left"> <strong>Câu 1 - 5 </strong></td>
+            <td align="left">Dạng trắc nghiệm</td>
+            <td align="left">Mỗi câu trả lời đúng thí sinh được 4 điểm</td>
+          </tr>
+           <tr>
+            <td align="left"><strong>Câu 6 - 13</strong></td>
+            <td align="left">Dạng trả lời ngắn</td>
+            <td align="left">Mỗi câu trả lời đúng thí sinh được 5 điểm</td>
+          </tr>
+           <tr>
+            <td align="left"><strong>Câu 14 – 15</strong></td>
+            <td align="left">Dạng trả lời chi tiết</td>
+            <td align="left">Mỗi lời giải đầy đủ được 20 điểm. Các lời giải chưa
+hoàn thành vẫn được cho điểm thành phần</td>
+          </tr>
+          
+          
+        </tbody>
+      </table>
+        </div>
+      </div>      
+    </div>  
+ 
+  </div>
+  
+  
+</div>
+
+<div id="prize" class="container-fluid text-center bg-grey">
+  <h2>Giải thưởng</h2><br>
+  <h4>Kết quả bài thi của thí sinh sẽ được thẩm định và phê duyêt bởi Ủy ban điều hành IMAS
+quốc tế. <br>Giải thưởng được xếp theo kết quả thi của thí sinh so với các thí sinh cùng độ tuổi
+tham dự thi tại Việt Nam.</h4>
+  <div class="row text-center slideanim">
+        <div class="col-sm-6 col-xs-12">
+      <div class="panel panel-default text-center">
+        <div class="panel-heading">
+          <h2 class="text-white">Vòng 1</h2>
+        </div>
+        <div class="panel-body">
+          <p class="text-justify">Để khuyến khích và ghi nhận kết quả đạt được của thí sinh, Ban tổ chức IMAS
+quốc tế sẽ trao các chứng nhận tham dự cuộc thi.<br>
+Mỗi Giấy chứng nhận đều kèm theo Báo cáo đánh giá năng lực tư duy Toán học của
+từng thí sinh. Các loại chứng nhận bao gồm:</p>
+        </div>
+        <div class="panel-footer">
+           <table class="table yellow-heading">
+        <thead>
+        <th>Tên chứng nhận</th>
+        <th text-align="center">Mô tả</th>
+        
+        </thead>
+        <tbody>
+          <tr>
+            <td align="left"> <strong>Loại xuất sắc <br> <em>High Distinction</em> </strong></td>
+            <td align="left" valign="middle">Dành cho thí sinh có kết quả trong top 5%</td>
+          
+          </tr>
+           <tr>
+            <td align="left"><strong>Loại Giỏi<br><em>Distinction</em></strong></td>
+            <td align="left" valign="middle">Dành cho các thí sinh có kết quả từ top 6%-15%</td>
+           
+          </tr>
+           <tr>
+            <td align="left"><strong>Loại Khá <br> <em>Credit</em></strong></td>
+            <td align="left" valign="middle">Dành cho các thí sinh có kết quả từ top 16%-50%</td>
+            
+          </tr>
+          
+          
+        </tbody>
+      </table>
+      <br>
+      <br>
+        </div>
+      </div>      
+    </div> 
+           <div class="col-sm-6 col-xs-12">
+      <div class="panel panel-default text-center">
+        <div class="panel-heading">
+          <h2 class="text-white">Vòng 2</h2>
+        </div>
+        <div class="panel-body">
+          <p class="text-justify">Các thí sinh xuất sắc nhất sẽ được trao tặng Huy chương và Giấy chứng nhận do
+Ủy ban điều hành IMAS quốc tế trao tặng.</p>
+        </div>
+        <br>
+        <br>
+        <div class="panel-footer">
+           <table class="table yellow-heading">
+        <thead>
+        <th>Tên chứng nhận</th>
+        <th align="center">Mô tả</th>
+        
+        </thead>
+        <tbody>
+          <tr>
+            <td align="left"> <strong>Huy chương Vàng<br><em>Gold Medal</em></strong></td>
+            <td align="left">Dành cho thí sinh có kết quả trong top 5%</td>
+          
+          </tr>
+           <tr>
+            <td align="left"> <strong>Huy chương Bạc<br><em>Silver Medal</em></strong><br></td>
+            <td align="left">Dành cho các thí sinh có kết quả từ top 6% - 15%</td>
+           
+          </tr>
+           <tr>
+            <td align="left"> <strong>Huy chương Đồng<br><em>Bronze Medal</em></strong><br></td>
+            <td align="left">Dành cho các thí sinh có kết quả từ top 16%-30%</td>
+            
+          </tr>
+          
+          
+        </tbody>
+      </table>
+      <br>
+      <br>
+        </div>
+      </div>      
+    </div> 
+ 
+  </div>
+  
+  
+</div>
+
+
+<!-- Container (Contact Section) -->
+<div id="contact" class="container-fluid">
+  <h2 class="text-center">LIÊN HỆ</h2>
+  <div class="row">
+    <div class="col-sm-5">
+      <h3>BAN TỔ CHỨC IMAS VIỆT NAM</h3>
+      <p>Quỹ Phát triển giáo dục IEG</p>
+      <p><span class="glyphicon glyphicon-map-marker"></span> 128 Nguyễn Thái Học, Ba Đình, Hà Nội</p>
+      <p><span class="glyphicon glyphicon-phone"></span> 0981 048 228/ 04-7109 1099</p>
+      <p><span class="glyphicon glyphicon-envelope"></span> imas@ieg.vn</p>
+      <p><span class="glyphicon glyphicon-globe"></span> <a href="https://www.facebook.com/IMASVietNam/">IMAS Việt Nam Fanpage</a></p>
+    </div>
+    <div class="col-sm-7 slideanim">
+      <div class="row">
+       <iframe width="100%" height="450" frameborder="0" style="border:0"
+src="https://www.google.com/maps/embed/v1/place?q=place_id:ChIJ5e9ghZ-rNTER_LA-B3OZh0c&key=AIzaSyDYdO1TwJ4K81fhM3ctFdJy4ginfNQ3rfQ" allowfullscreen></iframe> 
+      </div>
+     
+    </div>
+  </div>
+</div>
+
+<!-- Add Google Maps -->
+
+
+
+
+
+<!--
+To use this code on your website, get a free API key from Google.
+Read more at: https://www.w3schools.com/graphics/google_maps_basic.asp
+-->
+
+<footer class="container-fluid text-center">
+  <a href="#homepage" title="To Top">
+    <span class="glyphicon glyphicon-chevron-up"></span>
+  </a>
+  <p>Copyrights reserved IEG - <a href="http://ieg.vn/" title="Visit IEG Homepage">ieg.vn</a></p>
+</footer>
+
+<script>
+
+
+  
+$(document).ready(function(){
+  	jQuery(window).on('scroll', function(){'use strict';
+		if ( jQuery(window).scrollTop() > 500 ) {
+			jQuery('#masthead').addClass('animated fadeInDown sticky');
+		} else {
+			jQuery('#masthead').removeClass('animated fadeInDown sticky');
+		}
+	});
+  // Add smooth scrolling to all links in navbar + footer link
+  /*$(".navbar a, footer a[href='#myPage']").on('click', function(event) {
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (900) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top - 80
+      }, 900, function(){
+   
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  }); */
+  $(".nav li a, footer a[href='#homepage']").click(function(event) {
+  		/* Act on the event */
+  		var target = $(this).attr('href');
+  		$('html, body').animate({
+                    scrollTop: $(target).offset().top - 60
+                }, 2000);
+  	});
+  $(window).scroll(function() {
+    $(".slideanim").each(function(){
+      var pos = $(this).offset().top;
+
+      var winTop = $(window).scrollTop();
+        if (pos < winTop + 600) {
+          $(this).addClass("slide");
+        }
+    });
+  });
+})
+</script>
+<script src="{{asset('/js/particles.min.js')}}"></script>
+<script src="{{asset('/js/app.js')}}"></script>
+
+</body>
+</html>
